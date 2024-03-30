7b7e0ac8cb5754d3960ba6f50bf0f03f70b504c7
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-03-30 23:37:27 +0700

    feat: change username

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,6 @@
 import { Mongo } from "@/db/db";
 import { IUser } from "@/shared/models/User";
+import mongoose, { Mongoose, MongooseError, Types } from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -16,4 +17,36 @@ export async function GET(req: NextRequest) {
       status: 200,
     }
   );
+}
+
+export async function PATCH(req: NextRequest) {
+  let session = req.cookies.get("session");
+
+  if (session === undefined)
+    return NextResponse.json({ message: "login required" }, { status: 401 });
+
+  let body: { username: string };
+  try {
+    body = await req.json();
+    if (body.username === undefined) throw new Error("no username");
+  } catch (err) {
+    return NextResponse.json({ message: "invalid body" }, { status: 400 });
+  }
+
+  let userId = new mongoose.Types.ObjectId(session.value);
+
+  const db = await Mongo.getInstance();
+  try {
+    await db
+      .collection<IUser>("users")
+      .findOneAndUpdate({ _id: userId }, { $set: { username: body.username } });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { message: (err as MongooseError).message },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({ message: "updated" }, { status: 200 });
 }
