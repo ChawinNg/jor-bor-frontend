9b7c68685245a4a4106547c3b8380de5bcf19dc0
ChawinNg <ik.chawin@gmail.com>
2024-03-31 16:30:09 +0700

    feat: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "ui-red": "#C3083F",
+        "ui-black": "#1A1A1D",
+        "ui-text-light": "#999999",
+        "ui-input": "#121212",
+        "ui-input-border": "#1D283A",
+      },
     },
   },
   plugins: [],
