dc4d5555d71cd9545dcf2fd863f01f66ae9ec90c
ChawinNg <ik.chawin@gmail.com>
2024-03-31 17:08:18 +0700

    feat: menu section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,15 @@
-import Image from "next/image";
+import MenuSection from "@/components/home/MenuSection";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-row justify-center items-center h-screen gap-x-2 select-none">
-      <div className="flex flex-col items-center gap-y-4 font-bold text-white">
-        <Image
-          src="/img/home/game-title.svg"
-          alt="game-title"
-          width={549}
-          height={227}
-        />
+    <MenuSection>
+      <Link href="/register">
         <button className="w-1/2 bg-ui-red py-3 rounded-xl">Register</button>
+      </Link>
+      <Link href="/login">
         <button className="w-1/2 bg-ui-red py-3 rounded-xl">Login</button>
-      </div>
-      <div>
-        <Image
-          src="/img/home/logo.svg"
-          alt="game-title"
-          width={522}
-          height={522}
-        />
-      </div>
-    </main>
+      </Link>
+    </MenuSection>
   );
 }
--- src/components/home/MenuSection.tsx
+++ src/components/home/MenuSection.tsx
@@ -1,3 +1,34 @@
-export default function MenuSection() {
-  return <div></div>;
+import React from "react";
+import Image from "next/image";
+interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  children?: React.ReactNode;
 }
+
+const MenuSection = React.forwardRef<HTMLInputElement, IProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <div className="flex flex-row justify-center items-center h-screen gap-x-2 select-none">
+        <div className="flex flex-col items-center gap-y-4 font-bold text-white">
+          <Image
+            src="/img/home/game-title.svg"
+            alt="game-title"
+            width={549}
+            height={227}
+          />
+          {children}
+        </div>
+        <div>
+          <Image
+            src="/img/home/logo.svg"
+            alt="game-title"
+            width={522}
+            height={522}
+          />
+        </div>
+      </div>
+    );
+  }
+);
+
+MenuSection.displayName = "MenuSection";
+export default MenuSection;
