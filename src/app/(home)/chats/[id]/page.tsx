8108b99bc9a4b8ca16dc080bf3973b8ad959c4e3
ChawinNg <ik.chawin@gmail.com>
2024-04-01 21:49:09 +0700

    chat box v1

--- src/app/(home)/chats/[id]/page.tsx
+++ src/app/(home)/chats/[id]/page.tsx
@@ -1,12 +1,14 @@
+import PrivateChat from "@/components/chat/PrivateChat";
 import PrivateChatList from "@/components/chat/PrivateChatList";
 
 export default function ChatPage({ params }: { params: { id: string } }) {
   return (
-    <div className="flex flex-col h-screen justify-center items-start gap-y-8">
+    <div className="flex flex-row h-screen justify-center items-center gap-y-8 gap-x-24">
       <div className="flex flex-col w-1/6 justify-center bg-black h-1/2 gap-y-6 py-4 items-center rounded-xl">
         <div className="text-4xl font-semibold py-2">Private Chat</div>
         <PrivateChatList id={params.id} />
       </div>
+      <PrivateChat id={params.id} />
     </div>
   );
 }
--- src/components/chat/PrivateChat.tsx
+++ src/components/chat/PrivateChat.tsx
@@ -1,3 +1,7 @@
 export default function PrivateChat({ id }: { id: string }) {
-    
+  return (
+    <div className="w-4/6">
+      <div className="w-full">{id}</div>
+    </div>
+  );
 }
