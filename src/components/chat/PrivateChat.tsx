45dd80fb779310978715f367562ba203edaf772d
ChawinNg <ik.chawin@gmail.com>
2024-04-02 20:54:01 +0700

    feat: message section

--- src/components/chat/PrivateChat.tsx
+++ src/components/chat/PrivateChat.tsx
@@ -1,8 +1,11 @@
+import MessageSection from "./MessageSection";
+
 export default function PrivateChat({ id }: { id: string }) {
   return (
-    <div className="w-4/6 flex flex-col h-5/6 gap-y-4">
+    <div className="w-4/6 flex flex-col h-5/6 gap-y-8">
       <div className="w-full text-2xl font-bold">{id}</div>
       <span className="w-full border-t border-ui-text-light"></span>
+      <MessageSection />
     </div>
   );
 }
