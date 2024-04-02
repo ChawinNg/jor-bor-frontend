45754138405c61861da2228ab1f63738a1935f13
ChawinNg <ik.chawin@gmail.com>
2024-04-02 12:38:43 +0700

    message box

--- src/components/chat/PrivateChat.tsx
+++ src/components/chat/PrivateChat.tsx
@@ -1,7 +1,8 @@
 export default function PrivateChat({ id }: { id: string }) {
   return (
-    <div className="w-4/6">
-      <div className="w-full">{id}</div>
+    <div className="w-4/6 flex flex-col h-5/6 gap-y-4">
+      <div className="w-full text-2xl font-bold">{id}</div>
+      <span className="w-full border-t border-ui-text-light"></span>
     </div>
   );
 }
